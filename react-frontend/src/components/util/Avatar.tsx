c593f48796081b6343962b85d309808b3176cadd
Victor <victorsantos12dima@gmail.com>
2023-10-27 20:11:58 -0300

    Diferenciacao de cor do usuario

--- react-frontend/src/components/util/Avatar.tsx
+++ react-frontend/src/components/util/Avatar.tsx
@@ -1,3 +1,5 @@
+import { useAuthUserQuery } from '../../api/endpoints/auth.endpoint';
+
 interface Props {
   src?: string;
   name: string;
@@ -5,21 +7,29 @@ interface Props {
   className?: string;
   onClick?: () => void;
   style?: {};
+  size?: string;
 }
 
-function Avatar(props: Props) {
-  const { src, name, title, className, onClick, style } = props;
+const Avatar = (props: Props) => {
+  const { src, name, title, className, onClick, style, size } = props;
+  const { data: User } = useAuthUserQuery();
+
   return (
-    <div
-      className={`relative grid shrink-0 cursor-pointer place-items-center overflow-hidden rounded-full bg-green-600 ${
-        className ?? 'h-8 w-8 border-[1px]'
-      }`}
-      title={title ?? name}
-      {...{ style, onClick }}
-    >
-      <div>{name.at(0)}</div>
-      {src && <img src={src} alt={name} className='absolute block h-full w-full object-cover' />}
-    </div>
+    <>
+      {User &&
+        <div
+          className={`relative grid shrink-0 cursor-pointer place-items-center overflow-hidden rounded-full 
+             ${User.username === name ? "bg-green-500" : "bg-amber-500"}
+             ${size} ${className ?? 'h-8 w-8 border-[1px]'
+            }`}
+          title={title ?? name}
+          {...{ style, onClick }}
+        >
+          <div>{name.at(0)}</div>
+          {src && <img src={src} alt={name} className='absolute block h-full w-full object-cover' />}
+        </div>
+      }
+    </>
   );
 }
 

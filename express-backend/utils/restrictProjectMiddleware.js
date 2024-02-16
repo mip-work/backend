6c5cd6bc218733ae1bd6d4a0dd102ffeb7b6908a
victorzix <linkgamesplays@gmail.com>
2024-02-15 23:15:49 -0300

    fix(rpm): fix error in project calls

--- express-backend/utils/restrictProjectMiddleware.js
+++ express-backend/utils/restrictProjectMiddleware.js
@@ -5,13 +5,13 @@ const client = new PrismaClient();
 exports.restrictProjectMiddleware = async (req, res, next) => {
   const projectId = Number(req.params.projectId ?? req.body.projectId ?? req.query.projectId);
   const { summary } = req.query
-  
+
   if (!projectId) return reject(res);
 
   const project = await client.member.findFirst({
-    where: { AND: { userId: req.user.uid, projectId } },
+    where: { AND: { userId: req.userId, projectId } },
   });
-  
+
   if (!project) return reject(res);
 
   req.customParams = { projectId, summary }

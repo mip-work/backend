21fea2a494147bd809cb7d44b33606a50d1c8ead
victorzix <linkgamesplays@gmail.com>
2024-02-16 23:33:44 -0300

    feat(app): last config for application

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './db/prisma.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: './',
+      envFilePath: './.env',
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}

dab13a5bca3933699e6a2731e9e63adfa5c7a55c
victorzix <linkgamesplays@gmail.com>
2024-02-16 22:29:33 -0300

    feat(infra): add app configuration and prisma integration

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -1,10 +1,24 @@
+import { PrismaClient } from '@prisma/client';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from 'src/app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => {
+
+  app.enableCors();
+  app.use(cookieParser());
+
+  await app.listen(3009, () => {
     console.log('a');
   });
 }
 bootstrap();
+
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL_PLANET_SCALE,
+    },
+  },
+});

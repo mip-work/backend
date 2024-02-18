77aee62eec922746398ace05ce3ba38c225bdec9
SauloCouto <saulocouto1221@gmail.com>
2024-02-18 12:48:09 -0300

    feat: created ViewUserDTO; created builder to return ViewUserDTO; created UpdateUserDTO; create directory utils with handler Exception;

---
+++ nest-backend/src/modules/User/builder/user.builder.ts
@@ -0,0 +1,10 @@
+import { User } from '../dtos/user';
+import { ViewUserDTO } from '../dtos/view-user.dto';
+
+export class UserBuilder {
+  static createViewUser(user: User): ViewUserDTO {
+    const { id, username, email, profileUrl, lastLoggedIn } = user;
+
+    return { id, username, email, profileUrl, lastLoggedIn };
+  }
+}

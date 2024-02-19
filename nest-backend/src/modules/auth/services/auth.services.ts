9f532b6357a87ee7e6f41e2fb331bcec9b9c3399
SauloCouto <saulocouto1221@gmail.com>
2024-02-19 19:44:49 -0300

    feat: create function login

--- nest-backend/src/modules/auth/services/auth.services.ts
+++ nest-backend/src/modules/auth/services/auth.services.ts
@@ -1,7 +1,23 @@
-import { Injectable } from '@nestjs/common';
-import { UserRepository } from 'src/modules/User/repositories/user.repository';
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { PayloadLoginDTO } from '../dtos/login-user.dto';
+import { UserServices } from 'src/modules/User/services/users.services';
+import { AuthBuilder } from '../builder/auth.builder';
+import { SavePayloadUser } from '../dtos/save-token-user.dto';
 
 @Injectable()
 export class AuthServices {
-  constructor(private userRepository: UserRepository) {}
+  constructor(private userService: UserServices) {}
+
+  async login({email, pwd}: PayloadLoginDTO) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user.pwd !== pwd) {
+      throw new UnauthorizedException("Credentials Invalid");
+    }
+
+    const payload: SavePayloadUser = AuthBuilder.createPayloadToSaveToken(user);
+
+    return payload; 
+  }
+
 }

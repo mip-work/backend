74bc911d330096d6c4b93438bcb5f0e679a92ba0
SauloCouto <saulocouto1221@gmail.com>
2024-02-19 19:46:30 -0300

    feat: create routes /login and /logout

--- nest-backend/src/modules/auth/controllers/auth.controllers.ts
+++ nest-backend/src/modules/auth/controllers/auth.controllers.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthServices } from '../services/auth.services';
 import { RestExceptionHandler } from 'src/utils/rest-exception-handler';
 import { RegisterUserDTO } from 'src/modules/User/dtos/requests/register-user.dto';
 import { UserServices } from 'src/modules/User/services/users.services';
+import { PayloadLoginDTO } from '../dtos/login-user.dto';
+import { rmSync } from 'fs';
+import { TokenJWT } from 'src/utils/token.utils';
 
 @Controller('auth')
 export class AuthController {
@@ -23,4 +26,20 @@ export class AuthController {
       RestExceptionHandler.handleException(err, res);
     }
   }
+
+  @Post('login')
+  async login(@Body() payload: PayloadLoginDTO, @Res() res: Response) {
+    try {
+      const loginPayload = await this.authService.login(payload);
+      return await TokenJWT.generateToken(loginPayload, res);
+    } catch (err) {
+      RestExceptionHandler.handleException(err, res);
+    }
+  }
+
+  @Post('logout')
+  async logout(@Res() res: Response) {
+    return res.clearCookie("session-user").status(HttpStatus.OK).json({ token: null, status: HttpStatus.OK, logger: false });
+  }
+
 }

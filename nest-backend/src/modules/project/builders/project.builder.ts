9f3550f7e7b8f55b3cc41ee619556336a6742bb8
victorzix <linkgamesplays@gmail.com>
2024-02-19 15:55:12 -0300

    feat(project): create methods added

---
+++ nest-backend/src/modules/project/builders/project.builder.ts
@@ -0,0 +1,9 @@
+import { Project } from 'src/modules/project/dtos/project.dto';
+import { ViewProjectDTO } from 'src/modules/project/dtos/responses/view-project.dto';
+
+export class ProjectBuilder {
+  static createProjectView(project: Project): ViewProjectDTO {
+    const { id, name, descr, repo } = project;
+    return { id, name, descr, repo };
+  }
+}

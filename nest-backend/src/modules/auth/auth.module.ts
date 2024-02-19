9b1a212f8503d5020bcd2332e4172add051b0a0b
Victor Raphael <linkgamesplays@gmail.com>
2024-02-19 15:14:43 -0300

    fix(user): fix imports in module

--- nest-backend/src/modules/auth/auth.module.ts
+++ nest-backend/src/modules/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/db/prisma.service';
 import { UserRepository } from '../User/repositories/user.repository';
-import { AuthService } from './services/auth.services';
+import { AuthServices } from './services/auth.services';
 import { AuthController } from './controllers/auth.controllers';
 
 @Module({
   controllers: [AuthController],
-  providers: [UserRepository, AuthService, PrismaService],
+  providers: [UserRepository, AuthServices, PrismaService],
 })
 export class AuthModule {}

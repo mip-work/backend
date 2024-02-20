33d0706e1f6e847deb976aaa8821b277f0f01886
SauloCouto <saulocouto1221@gmail.com>
2024-02-20 19:34:29 -0300

    refactor: injected JwtService of @nest/jwt

--- nest-backend/src/modules/middlewares/middleware-auth.ts
+++ nest-backend/src/modules/middlewares/middleware-auth.ts
@@ -1,9 +1,11 @@
 import { HttpStatus, Injectable, NestMiddleware, Next, Req, Res } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 import { NextFunction, Request, Response } from "express";
-import { JwtPayload, verify } from "jsonwebtoken";
+
 
 @Injectable()
 export class MiddlewareAuth implements NestMiddleware {
+    constructor(private jwtService: JwtService) {}
 
     use(@Req() req: Request, @Res() res: Response, @Next() next: NextFunction) {
         const token = req.headers.authorization;
@@ -11,21 +13,11 @@ export class MiddlewareAuth implements NestMiddleware {
             return res.status(HttpStatus.UNAUTHORIZED).json({ message: "Access not authorized" });
         }
 
-        const bearer = token.split(" ")[1];
+        const access_token = token.split(" ")[1];
 
-        verify(bearer, process.env.JWT_ACCESS_TOKEN_SECRET, (err, data) => {
-            if (err) {
-                return res.status(HttpStatus.UNAUTHORIZED).json({ message: "Access not authorized" });
-            }
-            const { id, username, email }: any = data;
-            
-            req.loggedUser = {
-                id,
-                username,
-                email
-            };
-            next();
-        })
+        this.jwtService.verifyAsync(access_token, {secret: process.env.JWT_ACCESS_TOKEN_SECRET})
+
+        next();
     }
 
 }

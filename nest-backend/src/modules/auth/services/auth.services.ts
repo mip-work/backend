d894d2767a8f38e2fc7869973aed69121cfd8269
SauloCouto <saulocouto1221@gmail.com>
2024-02-20 19:31:20 -0300

    refactor: using the @nest/jwt JwtService sign that receives a payload in the parameter and returns a token

--- nest-backend/src/modules/auth/services/auth.services.ts
+++ nest-backend/src/modules/auth/services/auth.services.ts
@@ -1,12 +1,13 @@
-import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PayloadLoginDTO } from '../dtos/login-user.dto';
 import { UserServices } from 'src/modules/User/services/users.services';
 import { AuthBuilder } from '../builder/auth.builder';
 import { SavePayloadUser } from '../dtos/save-token-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthServices {
-  constructor(private userService: UserServices) {}
+  constructor(private userService: UserServices, private jwtService: JwtService) {}
 
   async login({email, pwd}: PayloadLoginDTO) {
     const user = await this.userService.findByEmail(email);
@@ -17,7 +18,9 @@ export class AuthServices {
 
     const payload: SavePayloadUser = AuthBuilder.createPayloadToSaveToken(user);
 
-    return payload; 
+    const token = this.jwtService.sign(payload);
+
+    return token; 
   }
 
 }

c1e552b77c070a98c781a48950987a72c0b209ad
SauloCouto <saulocouto1221@gmail.com>
2024-02-21 20:39:51 -0300

    refactor: change name file

--- nest-backend/src/modules/auth/guards/auth.guard.ts
+++ nest-backend/src/modules/auth/guards/auth.guard.ts
@@ -1,10 +1,11 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Request } from "express";
+import { UserRepository } from "src/modules/User/repositories/user.repository";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private jwtService: JwtService) {}
+    constructor(private jwtService: JwtService, private userRepository: UserRepository) {}
 
     async canActivate(context: ExecutionContext) {
         const request = context.switchToHttp().getRequest();
@@ -17,14 +18,21 @@ export class AuthGuard implements CanActivate {
         try {
             const payload = await this.jwtService.verifyAsync(accessToken, { secret: process.env.JWT_ACCESS_TOKEN_SECRET });
 
-            request['user'] = payload;
+            const user = await this.userRepository.findById(payload.id);
+
+            if (!user) {
+                throw new UnauthorizedException("Access don't authorized");
+            }
+
+            const { pwd, ...rest } = user;
+            request['user'] = rest;
         } catch (err) {
             throw new UnauthorizedException("Access don't authorized");
         }
         return true;
     }
 
-    private extractTokenFromHeader(req: Request) {
+    public extractTokenFromHeader(req: Request) {
         const [type, token] = req.headers.authorization?.split(" ") ?? [];
         return type === "Bearer" ? token : undefined;
     }

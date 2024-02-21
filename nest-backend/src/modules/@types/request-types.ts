ec1e01b799064b4cd5f33a17b21588337955b954
SauloCouto <saulocouto1221@gmail.com>
2024-02-21 20:38:31 -0300

    refactor: change name field

--- nest-backend/src/modules/@types/request-types.ts
+++ nest-backend/src/modules/@types/request-types.ts
@@ -2,7 +2,7 @@ declare namespace Express {
     export interface Request {
         user: {
             id: string,
-            name: string,
+            username: string,
             email: string
         }
     }

a0d17a0ac206fbb6479888b5520dae13ff8c46e3
SauloCouto <saulocouto1221@gmail.com>
2024-02-21 01:07:24 -0300

    refactor: remove try catch

--- nest-backend/src/modules/auth/controllers/auth.controllers.ts
+++ nest-backend/src/modules/auth/controllers/auth.controllers.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Get, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthServices } from '../services/auth.services';
-import { RestExceptionHandler } from 'src/utils/rest-exception-handler';
 import { RegisterUserDTO } from 'src/modules/User/dtos/requests/register-user.dto';
 import { UserServices } from 'src/modules/User/services/users.services';
 import { PayloadLoginDTO } from '../dtos/login-user.dto';
@@ -19,24 +18,16 @@ export class AuthController {
   @UseGuards(RegisterGuard)
   @Post('register')
   async register(@Body() userDTO: RegisterUserDTO, @Res() res: Response) {
-    try {
-      const user = await this.userService.register(userDTO);
-      return res
-        .status(HttpStatus.CREATED)
-        .json({ data: user, status: HttpStatus.CREATED });
-    } catch (err) {
-      RestExceptionHandler.handleException(err, res);
-    }
+    const user = await this.userService.register(userDTO);
+    return res
+      .status(HttpStatus.CREATED)
+      .json({ data: user, status: HttpStatus.CREATED });
   }
 
   @Post('login')
   async login(@Body() payload: PayloadLoginDTO, @Res() res: Response) {
-    try {
-      const accessToken = await this.authService.login(payload);
-      return res.cookie('access_token', accessToken).status(HttpStatus.OK).json({ access_token: accessToken, status: HttpStatus.OK });
-    } catch (err) {
-      RestExceptionHandler.handleException(err, res);
-    }
+    const accessToken = await this.authService.login(payload);
+    return res.cookie('access_token', accessToken).status(HttpStatus.OK).json({ access_token: accessToken, status: HttpStatus.OK });
   }
 
   @Post('logout')

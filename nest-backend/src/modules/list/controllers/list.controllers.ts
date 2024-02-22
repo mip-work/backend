a2049f30599d05579f1737a1520f29e67314760f
victorzix <linkgamesplays@gmail.com>
2024-02-22 08:35:34 -0300

    feat(list): remove controller error handling logic

--- nest-backend/src/modules/list/controllers/list.controllers.ts
+++ nest-backend/src/modules/list/controllers/list.controllers.ts
@@ -13,7 +13,6 @@ import {
 import { CreateListDto } from '../dtos/requests/create-list.dto';
 import { ListServices } from '../services/list.services';
 import { Response } from 'express';
-import { RestExceptionHandler } from 'src/utils/rest-exception-handler';
 import { ApiTags } from '@nestjs/swagger';
 import { UpdateListDTO } from '../dtos/requests/update-list-dto';
 
@@ -23,59 +22,43 @@ export class ListControllers {
   constructor(private listService: ListServices) {}
   @Post()
   async create(@Body() @Req() request: CreateListDto, @Res() res: Response) {
-    try {
-      const list = await this.listService.create(request);
-      return res.status(HttpStatus.CREATED).json({
-        data: list,
-        status: HttpStatus.CREATED,
-      });
-    } catch (err) {
-      return RestExceptionHandler.handleException(err, res);
-    }
+    const list = await this.listService.create(request);
+    return res.status(HttpStatus.CREATED).json({
+      data: list,
+      status: HttpStatus.CREATED,
+    });
   }
   @Get('/:id')
   async get(@Param('id') id: string, @Res() res: Response) {
-    try {
-      const list = await this.listService.get(id);
-      return res.status(HttpStatus.OK).json({
-        data: list,
-        status: HttpStatus.OK,
-      });
-    } catch (err) {
-      return RestExceptionHandler.handleException(err, res);
-    }
+    const list = await this.listService.get(id);
+    return res.status(HttpStatus.OK).json({
+      data: list,
+      status: HttpStatus.OK,
+    });
   }
   @Delete('/:id')
   async delete(@Param('id') id: string, @Res() res: Response) {
-    try {
-      const list = await this.listService.delete(id);
-      return res.status(HttpStatus.OK).json();
-    } catch (err) {
-      return RestExceptionHandler.handleException(err, res);
-    }
+    await this.listService.delete(id);
+    return res.status(HttpStatus.OK).json();
   }
   @Get('/all/:id')
   async getAll(@Param('id') id: string, @Res() res: Response) {
-    try {
-      const list = await this.listService.getAll(id);
-      return res.status(HttpStatus.OK).json({
-        data: list,
-        status: HttpStatus.OK,
-      });
-    } catch (err) {
-      return RestExceptionHandler.handleException(err, res);
-    }
+    const list = await this.listService.getAll(id);
+    return res.status(HttpStatus.OK).json({
+      data: list,
+      status: HttpStatus.OK,
+    });
   }
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() request: UpdateListDTO, @Res() res: Response) {
-    try {
-      const list = await this.listService.update(id, request);
-      return res.status(HttpStatus.OK).json({
-        data: list,
-        status: HttpStatus.OK,
-      });
-    } catch (err) {
-      return RestExceptionHandler.handleException(err, res);
-    }
-  }  
+  async update(
+    @Param('id') id: string,
+    @Body() request: UpdateListDTO,
+    @Res() res: Response,
+  ) {
+    const list = await this.listService.update(id, request);
+    return res.status(HttpStatus.OK).json({
+      data: list,
+      status: HttpStatus.OK,
+    });
+  }
 }

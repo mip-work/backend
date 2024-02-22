036c17266f9dfc50da14b2451f393f104f045704
victorzix <linkgamesplays@gmail.com>
2024-02-22 08:33:49 -0300

    feat(members): add repositories methods for member module

--- nest-backend/src/modules/member/repositories/member.repository.ts
+++ nest-backend/src/modules/member/repositories/member.repository.ts
@@ -6,10 +6,40 @@ import { CreateMemberDto } from '../dtos/requests/create-member.dto';
 export class MemberRepository {
   constructor(private prisma: PrismaService) {}
 
-  async create(dto: CreateMemberDto) {
+  async addMember(dto: CreateMemberDto) {
     const member = await this.prisma.member.create({
       data: dto,
     });
+    return member;
+  }
+
+  async get(id: string) {
+    const member = await this.prisma.member.findFirst({
+      where: {
+        id,
+      },
+    });
+    return member;
+  }
+
+  async findInProject(id: string, projectId: string) {
+    const member = await this.prisma.member.findFirst({
+      where: { userId: id, projectId },
+    });
+    return member;
+  }
+
+  async listMembers(projectId: string) {
+    const members = await this.prisma.member.findMany({
+      where: {
+        projectId,
+      },
+    });
+    return members;
+  }
+
+  async delete(id: string) {
+    const member = await this.prisma.member.delete({ where: { id } });
     return member;
   }
 }

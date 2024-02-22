fd5babfdd9249d68700d47a3a0ada181c8761f44
Victor Raphael <linkgamesplays@gmail.com>
2024-02-22 18:55:24 -0300

    Nest/member module (#39)
    * feat(projects): create module
    
    * feat(project): set up swagger config
    
    * refactor(modules): add apiproperties & tags to request dtos
    
    * feat(prisma): change member model
    
    * feat(members): add create, read & delete methods
    
    * feat(members): add create, read & delete methods
    
    * feat(members): create dtos & enums for member module
    
    * feat(members): add repositories methods for member module
    
    * feat(member): configure member module
    
    * feat(projects): configure project module within member module
    
    * feat(list): remove controller error handling logic
    
    * feat(member) create show & list method

--- nest-backend/src/modules/member/controllers/member.controllers.ts
+++ nest-backend/src/modules/member/controllers/member.controllers.ts
@@ -1,15 +1,103 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateMemberDto } from '../dtos/requests/create-member.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { MemberServices } from '../services/member.services';
 import { ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
+import { DeleteMemberDto } from '../dtos/requests/delete-member.dto';
+import { AuthGuard } from 'src/modules/auth/guards/auth.guard';
+import { CreateMemberDto } from '../dtos/requests/create-member.dto';
+import { UpdateMemberDto } from '../dtos/requests/updateMember.dto';
 
 @ApiTags('Member')
 @Controller('member')
 export class MemberControllers {
   constructor(private memberService: MemberServices) {}
+
+  @UseGuards(AuthGuard)
   @Post()
-  async create(@Body() request: CreateMemberDto) {
-    const member = await this.memberService.create(request);
-    return member;
+  async create(
+    @Body() body: CreateMemberDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const member = await this.memberService.addMember(body, req.user.id);
+    return res.status(HttpStatus.CREATED).json({
+      data: member,
+      status: HttpStatus.CREATED,
+    });
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete()
+  async remove(
+    @Body() dto: DeleteMemberDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    await this.memberService.delete(dto, req.user.id);
+    return res.status(HttpStatus.OK).json({
+      message: 'Member removed',
+      status: HttpStatus.OK,
+    });
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/list/:projectId')
+  async listMembers(
+    @Param('projectId') projectId: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const members = await this.memberService.listMembers(
+      projectId,
+      req.user.id,
+    );
+
+    return res.status(HttpStatus.OK).json({
+      data: members,
+      status: HttpStatus.OK,
+    });
+  }
+
+  @UseGuards(AuthGuard)
+  @Patch()
+  async changeRole(
+    @Body() dto: UpdateMemberDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const member = await this.memberService.changeRole(dto, req.user.id);
+    return res.status(HttpStatus.OK).json({
+      data: member,
+      status: HttpStatus.OK,
+    });
+  }
+
+  @UseGuards(AuthGuard)
+  @Get(':projectId')
+  async getMember(
+    @Param('projectId') projectId: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const member = await this.memberService.showMember(
+      req.body.userId,
+      projectId,
+    );
+    return res.status(HttpStatus.OK).json({
+      data: member,
+      status: HttpStatus.OK,
+    });
   }
 }
--- nest-backend/src/modules/member/repositories/member.repository.ts
+++ nest-backend/src/modules/member/repositories/member.repository.ts
@@ -1,15 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../db/prisma.service';
 import { CreateMemberDto } from '../dtos/requests/create-member.dto';
+import { UpdateMemberDto } from '../dtos/requests/updateMember.dto';
 
 @Injectable()
 export class MemberRepository {
   constructor(private prisma: PrismaService) {}
 
-  async create(dto: CreateMemberDto) {
+  async addMember(dto: CreateMemberDto) {
     const member = await this.prisma.member.create({
       data: dto,
     });
+    return member;
+  }
+
+  async getByUserId(userId: string) {
+    const member = await this.prisma.member.findFirst({
+      where: {
+        userId,
+      },
+    });
+    return member;
+  }
+
+  async findInProject(userId: string, projectId: string) {
+    const member = await this.prisma.member.findFirst({
+      where: { userId, projectId },
+    });
+    return member;
+  }
+
+  async listMembers(projectId: string) {
+    const members = await this.prisma.member.findMany({
+      where: {
+        projectId,
+      },
+    });
+    return members;
+  }
+
+  async delete(id: string) {
+    const member = await this.prisma.member.delete({ where: { id } });
+    return member;
+  }
+
+  async changeRole(dto: UpdateMemberDto) {
+    const member = await this.prisma.member.update({
+      where: { id: dto.id, projectId: dto.projectId },
+      data: { role: dto.role },
+    });
+
     return member;
   }
 }

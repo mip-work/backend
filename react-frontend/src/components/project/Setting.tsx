061587accdebbb59dd5af70b2a8a287d9d66bae4
luan-nvg <luannnvg@gmail.com>
2024-02-22 09:37:53 -0300

    add input member

--- react-frontend/src/components/project/Setting.tsx
+++ react-frontend/src/components/project/Setting.tsx
@@ -1,15 +1,15 @@
-import { FieldError, FieldValues, useForm } from 'react-hook-form';
-import InputWithValidation from '../util/InputWithValidation';
-import MemberInput from './MemberInput';
+import { FieldError, FieldValues, useForm } from "react-hook-form";
+import InputWithValidation from "../util/InputWithValidation";
+import MemberInput from "./MemberInput";
 import {
   selectCurrentProject,
   useUpdateProjectMutation,
-} from '../../api/endpoints/project.endpoint';
-import { useParams } from 'react-router-dom';
-import { selectMembers } from '../../api/endpoints/member.endpoint';
-import { selectAuthUser } from '../../api/endpoints/auth.endpoint';
-import toast from 'react-hot-toast';
-import MembersDropdown from './MemberDropdown';
+} from "../../api/endpoints/project.endpoint";
+import { useParams } from "react-router-dom";
+import { selectMembers } from "../../api/endpoints/member.endpoint";
+import { selectAuthUser } from "../../api/endpoints/auth.endpoint";
+import toast from "react-hot-toast";
+import MembersDropdown from "./MemberDropdown";
 
 const Setting = () => {
   const [updateProject, { isLoading }] = useUpdateProjectMutation();
@@ -29,21 +29,31 @@ const Setting = () => {
   const isAdmin = members.filter(({ userId: uid }) => uid === u.id)[0].isAdmin;
 
   const onSubmit = async (formData: FieldValues) => {
-    if (formData.name === name && formData.descr === descr && formData.repo === repo) return;
+    if (
+      formData.name === name &&
+      formData.descr === descr &&
+      formData.repo === repo
+    )
+      return;
     await updateProject({ id, ...formData });
-    toast('Project setting updated');
+    toast("Project setting updated");
   };
 
   return (
-    <div className='mt-10 px-5 sm:px-10'>
-      <h1 className='mb-4 text-xl font-semibold text-c-text'>Project Setting</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className='flex max-w-[30rem] flex-col gap-4'>
+    <div className="mt-10 px-5 sm:px-10">
+      <h1 className="mb-4 text-xl font-semibold text-c-text">
+        Project Setting
+      </h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex max-w-[30rem] flex-col gap-4"
+      >
         <InputWithValidation
           defaultValue={name}
-          label='Name'
-          placeholder='project name'
-          register={register('name', {
-            required: { value: true, message: 'name must not be empty' },
+          label="Name"
+          placeholder="project name"
+          register={register("name", {
+            required: { value: true, message: "name must not be empty" },
           })}
           error={errors.name as FieldError}
           darkEnabled
@@ -51,34 +61,37 @@ const Setting = () => {
         />
         <InputWithValidation
           defaultValue={descr}
-          label='Description'
-          placeholder='project description'
-          register={register('descr')}
+          label="Description"
+          placeholder="project description"
+          register={register("descr")}
           error={errors.descr as FieldError}
           darkEnabled
           readOnly={!isAdmin}
         />
         <InputWithValidation
           defaultValue={repo}
-          label='Repository'
-          placeholder='github repo link'
-          register={register('repo')}
+          label="Repository"
+          placeholder="github repo link"
+          register={register("repo")}
           error={errors.repo as FieldError}
           darkEnabled
           readOnly={!isAdmin}
         />
-        <MembersDropdown />
-        <div className='mt-2'>
+        <MemberInput members={members} projectId={id} readOnly={!isAdmin} />
+        {/* <MembersDropdown /> */}
+        <div className="mt-2">
           {!isAdmin && (
-            <span className='block text-sm text-red-400'>
+            <span className="block text-sm text-red-400">
               * Only Admin can edit the project setting *
             </span>
           )}
           <button
             disabled={!isAdmin}
-            className={`btn mt-3 ${!isAdmin ? 'pointer-event-none cursor-not-allowed' : ''}`}
+            className={`btn mt-3 ${
+              !isAdmin ? "pointer-event-none cursor-not-allowed" : ""
+            }`}
           >
-            {isLoading ? 'saving changes...' : 'Save Changes'}
+            {isLoading ? "saving changes..." : "Save Changes"}
           </button>
         </div>
       </form>

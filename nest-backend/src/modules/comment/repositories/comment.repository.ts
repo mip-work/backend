590204bbcdc2f6f510778a89c3920eb799809d20
Clodomir Jr <106870350+jrfurt@users.noreply.github.com>
2024-03-04 15:45:22 -0300

    feat(comment): create comment repository

--- nest-backend/src/modules/comment/repositories/comment.repository.ts
+++ nest-backend/src/modules/comment/repositories/comment.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../db/prisma.service';
 import { CreateCommentDto } from '../dtos/requests/create-comment.dto';
+import { UpdateCommentDto } from '../dtos/requests/update-comment.dto';
 
 @Injectable()
 export class CommentRepository {
@@ -9,6 +10,50 @@ export class CommentRepository {
   async create(dto: CreateCommentDto) {
     const comment = await this.prisma.comment.create({
       data: dto,
+    });
+    return comment;
+  }
+
+  async update(id: string, dto: UpdateCommentDto) {
+    const comment = await this.prisma.comment.update({
+      where: { id },
+      data: dto,
+    });
+
+    return comment;
+  }
+
+  async get(id: string) {
+    const comment = await this.prisma.comment.findFirst({
+      where: { id },
+    });
+    return comment;
+  }
+
+  async getAll(issueId: string) {
+    const comment = await this.prisma.comment.findMany({
+      where: { issueId },
+    });
+    return comment;
+  }
+
+  async getUserCommentInIssue(userId: string, issueId: string) {
+    const comment = await this.prisma.comment.findMany({
+      where: { userId, issueId },
+    });
+    return comment;
+  }
+
+  async getByUserId(userId: string) {
+    const comment = await this.prisma.comment.findMany({
+      where: { userId },
+    });
+    return comment;
+  }
+
+  async delete(id: string) {
+    const comment = await this.prisma.comment.delete({
+      where: { id },
     });
     return comment;
   }

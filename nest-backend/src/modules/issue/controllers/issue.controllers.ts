6a1be866dca2215312a93c386fc14c7458ce9432
Victor Raphael <linkgamesplays@gmail.com>
2024-03-05 22:43:03 -0300

    Nest/issue new module (#49)
    * feat(projects): create module
    
    * feat(project): set up swagger config
    
    * refactor(modules): add apiproperties & tags to request dtos
    
    * feat(prisma): change member model
    
    * feat(members): add create, read & delete methods
    
    * feat(members): add create, read & delete methods
    
    * feat(members): create dtos & enums for member module
    
    * feat(members): add repositories methods for member module
    
    * feat(member): configure member module
    
    * feat(projects): configure project module within member module
    
    * feat(list): remove controller error handling logic
    
    * feat(member) create show & list method
    
    * feat(prisma): change important tables
    
    * feat(list): add delete method
    
    * feat(list): create & delete method
    
    * feat(list): update list service
    
    * feat(list): create list algorithm for ordenation
    
    * fix(error): fix HttpExceptionFilter
    
    * feat(prisma): fixed sprint & issue model
    
    * feat(issue): services & repository

--- nest-backend/src/modules/issue/controllers/issue.controllers.ts
+++ nest-backend/src/modules/issue/controllers/issue.controllers.ts
@@ -1,15 +1,36 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateIssueDto } from '../dtos/requests/create-issue.dto';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Param,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { IssueServices } from '../services/issue.services';
 import { ApiTags } from '@nestjs/swagger';
+import { CreateIssueReqDto } from '../dtos/requests/create-issue-req.dto';
+import { AuthGuard } from 'src/modules/auth/guards/auth.guard';
+import { Request, Response } from 'express';
 
 @ApiTags('Issue')
 @Controller('issue')
 export class IssueControllers {
   constructor(private issueService: IssueServices) {}
-  @Post()
-  async create(@Body() request: CreateIssueDto) {
-    const sprint = await this.issueService.create(request);
-    return sprint;
+
+  @UseGuards(AuthGuard)
+  @Post(':projectId')
+  async create(
+    @Body() body: CreateIssueReqDto,
+    @Param('projectId') projectId: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const issue = await this.issueService.create(body, req.user.id, projectId);
+    return res.status(HttpStatus.CREATED).json({
+      data: issue,
+      status: HttpStatus.CREATED,
+    });
   }
 }

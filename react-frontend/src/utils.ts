f74c10444bd6c7d4b115d62b4d74c42991b8ff3b
Rayan Muryell <rayan_muryell@hotmail.com>
2024-03-14 21:15:18 -0300

    feat: progressbar

--- react-frontend/src/utils.ts
+++ react-frontend/src/utils.ts
@@ -1,27 +1,27 @@
-import moment from 'moment';
+import moment from "moment";
 
 export function getTheme(): Theme {
-  const localTheme = localStorage.getItem('jira-clone-theme');
-  return localTheme ? JSON.parse(localTheme) : { mode: 'light' };
+  const localTheme = localStorage.getItem("jira-clone-theme");
+  return localTheme ? JSON.parse(localTheme) : { mode: "light" };
 }
 
-export function setTheme(mode: Theme['mode']) {
+export function setTheme(mode: Theme["mode"]) {
   localStorage.setItem(
-    'jira-clone-theme',
+    "jira-clone-theme",
     JSON.stringify({
-      mode: mode === 'light' ? 'dark' : 'light',
+      mode: mode === "light" ? "dark" : "light",
     })
   );
 }
 
 export const parseDate = (s: string) => moment(s).fromNow();
 
-export type Theme = { mode: 'light' | 'dark' };
+export type Theme = { mode: "light" | "dark" };
 
 export const types = [
-  { text: 'Task', icon: '/assets/task.svg', value: 0 },
-  { text: 'Bug', icon: '/assets/bug.svg', value: 1 },
-  { text: 'Review', icon: '/assets/review.svg', value: 2 },
+  { text: "Task", icon: "/assets/task.svg", value: 0 },
+  { text: "Bug", icon: "/assets/bug.svg", value: 1 },
+  { text: "Review", icon: "/assets/review.svg", value: 2 },
 ];
 
 export const priorities = [
